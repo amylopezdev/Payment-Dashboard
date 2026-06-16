a502f7b68f93ba189da8ac36d57ee64ee5f87087
Amy Lopez <amylopezdev@gmail.com>
2026-06-16 14:50:14 +0100

    add aria-pressed

--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,9 +1,12 @@
+import "../styles/FilterButton.css";
+
 const FilterButton = ({ setActiveFilter, status, activeFilter }) => {
   const isActive = activeFilter === status;
   return (
     <li>
       <button
         className={`payments-filters__btn${isActive ? " payments-filters__btn--active" : ""}`}
+        aria-pressed={isActive}
         onClick={() => {
           setActiveFilter(status);
         }}
